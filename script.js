16ee4fc2a3cdbb2f93c32133a108f12ec320829a
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-10-15 19:31:56 +0300

    github pages

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+
+    function getWaterAmount(arr) {
+
+
+    }
+
+
+
+    console.group('getWaterAmount');
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+    console.log('getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) => ', getWaterAmount([2,1,5,0,3,4,7,2,3,1,0]) )
+
+    console.groupEnd();
+};
